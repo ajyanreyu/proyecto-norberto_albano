9a7dd7392bbd6f7a2800f276f4527108551e6aa5
Yanes Reyes <horus_aj@hotmail.com>
2016-05-02 14:37:25 +0000

    Añadido el package.json y modificada las rutas de app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressLayouts = require('express-ejs-layouts');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
